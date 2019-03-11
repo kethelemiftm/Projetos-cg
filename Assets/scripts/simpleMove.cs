50bd423ed09a2f92c60b5dd7f405f8a79fa2b6c7
kethelemiftm <kethelemiftm@gmail.com>
2019-03-11 10:41:14 -0300

    incluindo limites

--- Assets/scripts/simpleMove.cs
+++ Assets/scripts/simpleMove.cs
@@ -6,6 +6,14 @@ public class simpleMove : MonoBehaviour
 {
     [SerializeField]
     private float SpeedFactor = 1f;
+    [SerializeField]
+private float LimitTop = 4.5f;
+[SerializeField]
+private float LimitBottom = -4.5f;
+[SerializeField]
+private float Limitleft = -8.5f;
+[SerializeField]
+private float LimititRight = 8.5f;
         void Start()
     {
     
@@ -20,5 +28,20 @@ public class simpleMove : MonoBehaviour
         Vector3 velocity = new Vector3 (horizontalInput, verticalInput, 0f) * SpeedFactor;
 
         transform.position = transform.position + velocity * Time.deltaTime;
+
+        if (transform.position.y < LimitBottom) {
+            transform.position = new Vector3 (transform.position.x, LimitBottom,transform.position.z);
+        }
+        if (transform.position.y > LimitTop){
+            transform.position = new Vector3 (transform.position.x, LimitTop,transform.position.z);
+        }
+        if (transform.position.x < Limitleft){
+             transform.position = new Vector3 (Limitleft, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > LimititRight) {
+             transform.position = new Vector3 (LimititRight, transform.position.y, transform.position.z);
+    
     }
+ }
 }
+
